2fc3df45c4e593e2f95314409ac050181fda8238
nilsboy <nils@boysen.info>
2017-02-08 12:13:54 +0100

    misc

--- index.js
+++ index.js
@@ -1,69 +1,180 @@
-// TODO unredirect console.log?
+#!/usr/bin/env node
+
+// TODO: option to set pattern layout
+// TODO: dump config if LOG_DWIM_DUMP_CONFIG is set or at trace level?
+// TODO add color - the stdout appender has color already build in
+// TODO: to add line and column of log statement:
+//   checkout: https://github.com/ww24/log4js-node-extend
+// TODO: add stacktrace
+// TODO: allow to call with a function to execute but also to print
+// TODO: env vars based on app name
+// TODO: add option for sync: https://github.com/nomiddlename/log4js-node/wiki/Date%20rolling%20file%20appender%20-%20with%20synchronous%20file%20output%20modes
 
 "use strict;"
 
-let logLevel = process.env.LOG_LEVEL || `INFO`
-console.log(`loglevel: ${logLevel}`)
+// get calling modules package.json
+const mainPackage = require.main.require('./package')
 
-logLevel = logLevel.toLowerCase()
-
+const paths = require('env-paths')(mainPackage.name)
 const log4js = require('log4js');
 const util = require('util')
-
+const fs = require('fs')
+const path = require('path')
+const expandTilde = require('expand-tilde')
+const mkdir = require(`mkdirp`).sync
+const _ = require('lodash')
 
 var config = {
     "appenders": [
     ]
 }
 var consoleAppenderConfig = {
+  "category": [ "main" ],
   "type": "stderr",
   "layout": {
+    "type": "pattern"
+    , pattern: "%d{hh:mm} %-5p> %m"
+    , example: `INFO> message`
+  }
+}
+
+var fileAppenderConfig = {
+  "type": "dateFile",
+  "usefsync": true,
+  "absolute": true,
+  "pattern": ".dd",
+  // add pattern to first file too
+  "alwaysIncludePattern": false, 
+  category: [ 'main' ],
+  "layout": {
     "type": "pattern",
-    "pattern": "%d%h %p> %m%n",
+    "pattern": "%d %h %p> %m",
   }
 }
-
-var consoleConfig = {
-  info: {
-    example: `INFO> message`
-    , pattern: "%d{hh:mm} %-5p> %m",
-  }
-  , debug: {
-    example: `2017/01/09 00:17:04 [11104] DEBUG /home/file 59 class> message`
-    , pattern: "%d{ISO8601_WITH_TZ_OFFSET} [%x{pid}] %p %m"
-    , tokens: {
-      "pid" : function() { return process.pid }
-    }
-  }
-}
-
-// TODO: option to set pattern layout
-// consoleAppenderConfig.layout.pattern = consoleConfig[logLevel].pattern
-consoleAppenderConfig.layout.pattern = consoleConfig[`info`].pattern
-consoleAppenderConfig.layout.tokens = consoleConfig[`info`].tokens
 
 config.appenders.push(consoleAppenderConfig)
 
-log4js.configure(config, {});
-
-/* TODO app */
-let logger = log4js.getLogger("app");
+let logger = log4js.getLogger(`main`)
+let logLevel = process.env.LOG_LEVEL || `INFO`
+console.error(`using loglevel: ${logLevel}`)
+logLevel = logLevel.toLowerCase()
 logger.setLevel(logLevel)
 
+log4js.configure(config)
 var exports = {
-  DIE() {
+  EXIT() {
     logger.fatal(...Array.prototype.slice.call(arguments))
     process.exit(1)
   },
   DUMP(object) {
-    logger[logLevel](`\n` + util.inspect(object, {depth: null}))
-  }
+    logger[logLevel](`DUMP:\n  ` + util.inspect(object, {depth: null}))
+  },
+  createLogFileName(logFilePath = mainPackage.name) {
+    if(!logFilePath) {
+      logFilePath = mainPackage.name
+    }
+    logFilePath = expandTilde(logFilePath)
+    
+    if(!path.isAbsolute(logFilePath)) {
+      logFilePath = path.join(paths.log, logFilePath)
+    }
+
+    mkdir(path.dirname(logFilePath))
+
+    if(path.extname(logFilePath) !== `.log`) {
+      logFilePath += `.log`
+    }
+  },
+  setLogFile(logFilePath) {
+    if(!logFilePath) {
+      logFilePath = mainPackage.name
+    }
+    logFilePath = expandTilde(logFilePath)
+    
+    if(!path.isAbsolute(logFilePath)) {
+      logFilePath = path.join(paths.log, logFilePath)
+    }
+
+    mkdir(path.dirname(logFilePath))
+
+    if(path.extname(logFilePath) !== `.log`) {
+      logFilePath += `.log`
+    }
+
+    if (config.appenders[1]) {
+      logger.warn(`Replacing already set logfile: ${config.appenders[1].filename} - with: ${logFilePath}`)
+    } else {
+      logger.info(`Setting logfile to ${logFilePath}`)
+    }
+
+    fileAppenderConfig.filename = logFilePath
+    config.appenders[1] = fileAppenderConfig
+    log4js.configure(config)
+
+    logger.info(`Logfile set to: ${logFilePath}`)
+  },
 }
+
+exports.logger = exports
 
 for (let m of [`trace`, `debug`, `info`, `warn`, `error`, `fatal`]) {
   exports[m.toUpperCase()] = function() {
+    if (!logger[`is` + _.startCase(m) + `Enabled`]()) {
+      return
+    }
+
     return logger[m](...Array.prototype.slice.call(arguments))
   }
+}
+
+process.on('SIGINT', () => {
+  logger.fatal(`sigint 1`)
+})
+
+process.on('SIGINT', () => {
+  logger.fatal(`sigint 2`)
+  process.exit(1)
+})
+
+const sigintListener = () => {
+  logger.fatal(`received SIGINT - exiting`)
+  process.exit(1)
+}
+
+const uncaughtExceptionListener = (err) => {
+  logger.fatal(`Exiting due to uncaught exception: ${err}`)
+  // log4js.shutdown()
+  throw err
+  // process.exit(1)
+}
+
+
+const sighubListener = () => {
+  logger.warn(`received SIGHUB`)
+}
+
+function warningListener(warning) {
+  logger.warn(warning)
+}
+
+// TODO: handle other events too?
+addAsFirstListener(`SIGINT`, sigintListener) 
+addAsFirstListener(`SIGHUB`, sighubListener) 
+addAsFirstListener(`uncaughtException`, uncaughtExceptionListener) 
+addAsFirstListener(`warning`, warningListener)
+
+// if no other listeners: exit.
+function addAsFirstListener(signal, handler) {
+  let listeners = process.listeners(signal)
+  process.removeAllListeners(signal)
+  
+  for (const listener of [handler, ...listeners]) {
+    process.on(signal, listener)
+  }
+}
+
+if (process.env.LOG_FILE) {
+  exports.setLogFile(process.env.LOG_FILE)
 }
 
 module.exports = exports
---
+++ test/test.js
@@ -0,0 +1,13 @@
+/* global it */
+
+'use strict'
+
+var assert = require('assert')
+var {logger} = require('../')
+
+logger.INFO(`hahahaha`)
+
+// it('xx', () =>
+//   assert.ok(logger.createLogFileName().match(/bla/))
+// )
+
